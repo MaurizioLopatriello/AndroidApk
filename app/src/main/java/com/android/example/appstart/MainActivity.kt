856962e5696915baafec6f829c294f23f45415b8
Maurizio Lopatriello <Carlolp69@gmail.com>
2022-09-26 18:16:02 +0200

    Appstart now has navigation between 2 fragments

--- app/src/main/java/com/android/example/appstart/MainActivity.kt
+++ app/src/main/java/com/android/example/appstart/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.android.example.appstart
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //val navigationView =findNavController(R.id.nav_host_fragment)
+        //return navigationView.navigate(androidx.navigation.fragment.R.id.nav_host_fragment_container)
+
     }
 }
--- app/src/main/java/com/android/example/appstart/fragment_second.kt
+++ app/src/main/java/com/android/example/appstart/fragment_second.kt
@@ -15,10 +15,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [FirstFragment.newInstance] factory method to
+ * Use the [fragment_second.newInstance] factory method to
  * create an instance of this fragment.
  */
-class FirstFragment : Fragment() {
+class fragment_second : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -36,14 +36,15 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_second, container, false)
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val button = view.findViewById<Button>(R.id.button)
-        button.setOnClickListener{
-            findNavController().navigate(R.id.action_firstFragment_to_fragment_second)
+        val button2= view.findViewById<Button>(R.id.ButtonBack)
+        button2.setOnClickListener{
+            findNavController().navigate(R.id.action_fragment_second_to_firstFragment)
         }
     }
 
@@ -54,12 +55,12 @@ class FirstFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment secondFragment.
+         * @return A new instance of fragment fragment_second.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            FirstFragment().apply {
+            fragment_second().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
